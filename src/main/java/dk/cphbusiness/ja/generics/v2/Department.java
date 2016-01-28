d9fcda033b477b2dcd046dedc1dadefa229d7a38
Anders Kalhauge <anders@kalhauge.dk>
2016-01-28 08:49:14 +0100

    Commit at version 2.0

---
+++ src/main/java/dk/cphbusiness/ja/generics/v2/Department.java
@@ -0,0 +1,33 @@
+package dk.cphbusiness.ja.generics.v2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department {
+  private final String code;
+  private String name;
+  
+  private final List<Employee> employees = new ArrayList<Employee>();
+
+  public double totalSalary() {
+    double total = 0.0;
+    for (int i = 0; i < employees.size(); i++) {
+      Employee employee = employees.get(i);
+      total = total + employee.getSalary();
+      }
+    return total;
+    }
+  
+  public List<Employee> getEmployees() { return employees; }
+  
+  public Department(String code, String name) {
+    this.code = code;
+    this.name = name;
+    }
+
+  public String getCode() { return code; }
+
+  public String getName() { return name; }
+  public void setName(String value) { this.name = value; }
+
+  }
---
+++ src/main/java/dk/cphbusiness/ja/generics/v2/Employee.java
@@ -0,0 +1,35 @@
+package dk.cphbusiness.ja.generics.v2;
+
+public class Employee {
+  private final int id;
+  private String name;
+  private double salary;
+  private Department department;
+
+  public Employee(int id, String name, double salary, Department department) {
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+    setDepartment(department);
+    }
+
+  public int getId() { return id; }
+
+  public String getName() { return name; }
+  public void setName(String value) { name = value; }
+
+  public double getSalary() { return salary; }
+  public void setSalary(double value) { salary = value; }
+
+  public Department getDepartment() { return department; }
+  
+  public final void setDepartment(Department value) { 
+    if (department != null) department.getEmployees().remove(this);
+    department = value;
+    // Now Department#employees only accepts Employees.
+    if (department != null) department.getEmployees().add(this);
+    }
+  
+  }
+
+
